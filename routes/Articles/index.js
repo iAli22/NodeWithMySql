bc7e509a2916322ce2f29982bb859b411c6d533c
iAli <iali@alis-MacBook-Air.local>
2020-07-29 21:44:44 +0200

    init Routes

--- routes/Articles/index.js
+++ routes/Articles/index.js
@@ -4,12 +4,15 @@ const uuid = require("uuid");
 
 // @ GET
 // All Articles
-router.get("/all", async (req, res) => {
-  await sqlConnection.query("SELECT * FROM articles", (err, result, fields) => {
-    if (err) throw err;
+router.get("/all/:page", async (req, res) => {
+  await sqlConnection.query(
+    `SELECT * FROM articles ORDER BY created_at DESC LIMIT ${req.params.page}`,
+    (err, result, fields) => {
+      if (err) throw err;
 
-    res.json(result);
-  });
+      res.json(result);
+    }
+  );
 });
 
 // @ GET
@@ -27,7 +30,7 @@ router.get("/:id", (req, res) => {
 
 // @ DELETE
 // DELETE Article BY ID
-router.delete(":id", (req, res) => {
+router.delete("/:id", (req, res) => {
   const sql = `DELETE FROM articles WHERE id = '${req.params.id}'`;
   sqlConnection.query(sql, (err, result, fields) => {
     if (err) throw err;
@@ -47,6 +50,21 @@ router.post("/create", (req, res) => {
     if (err) throw err;
 
     res.json(result);
+  });
+});
+
+// @ UPDATE
+// Update Article BY ID
+router.put("/:id", (req, res) => {
+  const { title } = req.body;
+
+  const sql = `UPDATE articles SET title = '${title}' WHERE id = '${req.params.id}'`;
+
+  // const sql = `DELETE FROM articles WHERE id = '${req.params.id}'`;
+  sqlConnection.query(sql, (err, result, fields) => {
+    if (err) throw err;
+    res.send(result);
+    console.log(result);
   });
 });
 
